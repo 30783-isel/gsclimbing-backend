1d8ad393cd4ed03ecbbf5096f6a4bdce1c00204f
janganga <janganga@gmail.com>
2022-04-16 10:38:19 +0100

    forget password

--- src/main/java/com/gsclimbing/security/AuthController.java
+++ src/main/java/com/gsclimbing/security/AuthController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -12,11 +13,16 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gsclimbing.commons.PasswordGenerator;
 import com.gsclimbing.commons.ResponseMessage;
+import com.gsclimbing.database.entity.User;
+import com.gsclimbing.database.service.UserService;
+import com.gsclimbing.email.SendEmail;
 
 @CrossOrigin(origins = "*", methods = {RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 
@@ -25,6 +31,9 @@ import com.gsclimbing.commons.ResponseMessage;
 public class AuthController {
 
 	Logger log = LoggerFactory.getLogger(getClass());
+	
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping("/basicauth")
 	public AuthenticationBean setAuthentication() {
@@ -42,5 +51,27 @@ public class AuthController {
 		}
 		return ResponseEntity.status(HttpStatus.OK).body(ResponseMessage.builder().message(roleStr.getAuthority()).build());
 	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/forgetpassword/{email}")
+	public ResponseEntity<?> forgetpassword(final @PathVariable String email) {
+		String message = null;
+		User user = userService.getUserByEmail(email);
+		if (user != null) {
+			String password = PasswordGenerator.generateCommonLangPassword();
+			String subject = "New password";
+			message = "Your new password is " + password;
+			byte[] bytes = null;
+			SendEmail runnable = new SendEmail(user.getEmail(), subject, message, null, null);
+			Thread t = new Thread(runnable);
+			t.start();
+			user.setPassword(password);
+			userService.updateUser(user);
+			return null;
+		} else {
+			message = "This email is not on our database;";
+			return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
 	
 }
